896a0969dfd19f4771013759b914850737dc011a
saul <otanisau@gmail.com>
2025-06-13 20:16:50 -0400

    feat(ci): Implement FinOps Guardian bot with Infracost action

--- main.tf
+++ main.tf
@@ -15,3 +15,12 @@ resource "aws_s3_bucket" "website_bucket" {
     Environment = "Dev"
   }
 }
+resource "aws_db_instance" "portfolio_db" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "portfolio_user"
+  password             = "MustBeSecret"
+  skip_final_snapshot  = true
+}
